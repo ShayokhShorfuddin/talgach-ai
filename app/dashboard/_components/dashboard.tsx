fe3141d557e30203101d64a49063b79158a70649
Shayokh <shayokhshuvro14@gmail.com>
2025-12-06 12:48:33 +0600

    add AI CV score

--- app/dashboard/_components/dashboard.tsx
+++ app/dashboard/_components/dashboard.tsx
@@ -1,3 +1,4 @@
+import { AICVScore } from './cards/ai-cv-scrore';
 import { CVStrengthCard } from './cards/cv-strength-card';
 import { Heading } from './heading';
 
@@ -6,8 +7,9 @@ export function Dashboard() {
     <section className="px-5 font-sans">
       <Heading />
 
-      <div className="grid grid-cols-4 mt-10 gap-5">
+      <div className="grid grid-cols-4 mt-10 gap-4">
         <CVStrengthCard />
+        <AICVScore />
       </div>
     </section>
   );
